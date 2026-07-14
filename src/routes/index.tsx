f97c51ea503444b09a484b7495280fa5e613cbda
Lexmanthefirst <lexmanthefirst@gmail.com>
2026-07-14 12:36:50 +0100

    Remove feature cards in favor of I engineer for speed  accordion

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,5 @@
 import { createFileRoute } from '@tanstack/react-router'
 import { Hero } from '../components/Hero'
-import { Features } from '../components/Features'
 import { TechSpecs } from '../components/TechSpecs'
 import { Projects } from '../components/Projects'
 import { Experience } from '../components/Experience'
@@ -14,7 +13,6 @@ function Home() {
   return (
     <>
       <Hero />
-      <Features />
       <TechSpecs />
       <Projects />
       <Experience />
